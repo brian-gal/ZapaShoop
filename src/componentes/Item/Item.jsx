295c1011390c171efbb51cb6dea94b41e6bc0bea
brian <briamgalvez2013@gmail.com>
2024-09-05 03:41:15 -0300

    retire el boton detalles y hice que al tocar el contenedor item te lleve a los detalles

--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -3,27 +3,26 @@ import './Item.css';
 
 const Item = ({ id, title, image, price, count }) => {
     return (
-        <article className="product-container">
-            <picture>
-                <img src={image} alt={title} />
-            </picture>
-            <h2 className="ItemHeader" >
-                {title}
-            </h2>
-            <div>
-                <p className="price">Precio: {price}</p>
-                <p className="stock">
-                    {count === 0
-                        ? 'Sin stock'
-                        : count === 1
-                            ? 'Último disponible'
-                            : `Stock: ${count}`}
-                </p>
-            </div>
-            <section>
-                <Link to={`/item/${id}`} className="details-button">Ver detalles</Link>
-            </section>
-        </article>
+        <Link to={`/item/${id}`} className="details-button">
+            <article className="product-container">
+                <picture>
+                    <img src={image} alt={title} />
+                </picture>
+                <h2 className="ItemHeader" >
+                    {title}
+                </h2>
+                <div>
+                    <p className="price">Precio: {price}</p>
+                    <p className="stock">
+                        {count === 0
+                            ? 'Sin stock'
+                            : count === 1
+                                ? 'Último disponible'
+                                : `Stock: ${count}`}
+                    </p>
+                </div>
+            </article>
+        </Link>
     )
 }
 
